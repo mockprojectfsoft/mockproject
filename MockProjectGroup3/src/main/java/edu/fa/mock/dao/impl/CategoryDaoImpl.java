667020cfe791702b52afef3c01ed92f21fd1856e
mockprojectfsoft <mockproject.fsoft@gmail.com>
2018-06-28 10:10:59 +0700

    update 28/6

--- MockProjectGroup3/src/main/java/edu/fa/mock/dao/impl/CategoryDaoImpl.java
+++ MockProjectGroup3/src/main/java/edu/fa/mock/dao/impl/CategoryDaoImpl.java
@@ -1,13 +1,13 @@
 package edu.fa.mock.dao.impl;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import edu.fa.mock.dao.CategoryDao;
 import edu.fa.mock.model.Category;
 
 @Transactional
-@Repository
+@Component
 public class CategoryDaoImpl extends DaoImplUtils<Category> implements CategoryDao {
 
 }
--- MockProjectGroup3/src/main/java/edu/fa/mock/dao/impl/DaoImplUtils.java
+++ MockProjectGroup3/src/main/java/edu/fa/mock/dao/impl/DaoImplUtils.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,7 +20,7 @@ import edu.fa.mock.dao.DaoUtils;
  * @param <E>
  */
 @Transactional
-@Repository
+@Component
 public abstract class DaoImplUtils<E> implements DaoUtils<E> {
 
 	private Class<E> e;
